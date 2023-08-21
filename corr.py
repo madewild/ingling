a6ea2d3e48752d5587b4840529e14d677dbd3d1a
madewild <max.de.wilde@ulb.be>
2023-08-21 09:52:39 +0200

    some linting

--- corr.py
+++ corr.py
@@ -7,12 +7,12 @@ import sys
 import spacy
 from spacy.matcher import Matcher
 
-sess = sys.argv[1]
+SESS = sys.argv[1]
 
-def match_pattern(model, pattern):
+def match_pattern(spacy_model, pattern):
     """Take a pattern as input and return matches (with longest match)"""
     res = set()
-    nlp = spacy.load(model)
+    nlp = spacy.load(spacy_model)
     doc = nlp(text)
     matcher = Matcher(nlp.vocab)
     matcher.add("", pattern)
@@ -21,13 +21,13 @@ def match_pattern(model, pattern):
     for span in spacy.util.filter_spans(spans):
         match_string = str(span).lower()
         res.add(match_string)
-    return(res)
+    return res
 
-text = open("data/test.txt").read()
+text = open("data/test.txt", encoding="utf-8").read()
 models = ["fr_core_news_sm", "fr_core_news_md", "fr_core_news_lg", "fr_dep_news_trf"]
 models = models[-1:]
 
-root = f"data/2023/sess{sess}/"
+root = f"data/2023/sess{SESS}/"
 folders = os.listdir(root)
 print(f"\n{len(folders)} assignments found\n")
 for folder in sorted(folders):
@@ -41,12 +41,12 @@ for folder in sorted(folders):
         print(f"More than one JSON file found for {student}")
         sys.exit()
     else:
-        json_file = open(f"{root}{folder}/{json_files[0]}").read()
+        json_file = open(f"{root}{folder}/{json_files[0]}", encoding="utf-8").read()
         patterns = json.loads(json_file)
         scores = []
         for model in models:
             results = match_pattern(model, patterns)
-            expected = [l.strip() for l in open("data/gold.txt").readlines()]
+            expected = [l.strip() for l in open("data/gold.txt", encoding="utf-8").readlines()]
             score = 0
             for phrase in expected:
                 if phrase in results:
